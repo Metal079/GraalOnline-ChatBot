3ac80969a255eb894d08a2540c929c406e91f84b
Metal079 <Metal145965@gmail.com>
2017-05-30 18:29:16 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def find_notification(): #looks for a waiting PM and clicks it
 		if image is not None:
 			print('Found a waiting message')
 			pyautogui.click(image)
-			time.sleep(.1)
+			time.sleep(.2)
 			break
 
 
@@ -95,7 +95,7 @@ def read_message(text_coords): # Reads PM for numbers and sets option the number
 	elif pyautogui.locateCenterOnScreen('5.png',region = text_coords,  confidence = .8, grayscale = True):
 		option = '5'
 		print('Player messaged 5')
-	elif pyautogui.locateCenterOnScreen('6.png',region = text_coords,  confidence = .8, grayscale = True):
+	elif pyautogui.locateCenterOnScreen('6.png',region = text_coords,  confidence = .9, grayscale = True):
 		option = '6'
 		print('Player messaged 6')
 	elif pyautogui.locateCenterOnScreen('7.png',region = text_coords,  confidence = .8, grayscale = True):
@@ -126,31 +126,69 @@ def response(placement): # All the possible responses the bot can give a player
 	elif placement == '1':
 		return "The bounty quest allows you to hunt mobs for cash! Location: Castle, steward's room(to the right in the throne room) [PM 1 for specifics, PM 2 for TIPS, PM 3 for possible bounties]"
 	elif placement == '11':
-		return 'Section under construction please check back later. PM "Metal" to suggest things [PM 0 to reset choices]'
+		return 'Section under construction please check back later. PM "Metal" to suggest things [PM 0 to reset bot]'
 	elif placement == '12':
 		return '1. The bounty box will "drop"(stop following you, and will not pick up anything) after every kill/capture you get, and will require you to call it, or run over it to pick it up again. [PM 1 for more info, PM 0 to reset choices]'
 	elif placement == '13':
-		return '100 green blobs, 20 Lizardons, 75 Pyrats, 75 Rebel soldiers(regular green baddy), 60 dark blobs, 60 rats, 75 snakes, 75 bats, 75 bandits, 80 spiders, 50 archers, or 50 crabs. [PM 0 to reset choices]'
+		return '100 green blobs, 20 Lizardons, 75 Pyrats, 75 Rebel soldiers(regular green baddy), 60 dark blobs, 60 rats, 75 snakes, 75 bats, 75 bandits, 80 spiders, 50 archers, or 50 crabs. [PM 0 to reset bot]'
 
 	elif placement == '2':
-		return 'Section under construction please check back later. PM "Metal" to suggest things [PM 0 to reset choices]'
+		return '[PM 1 for Mantis info, 2 for Grasshopper, 3 for ladybug, 4 for Bumblebee, 5 for Dragonfly, or 6 for selling bugs]'
+	elif placement == '21':
+		return 'Flies from bush to bush and is worth 52 Gralats. Catching them is pretty easy, just turn towards the bush it is on and swing. Delta and Mod are my favorite places to find these bugs. But they are all over the map. [PM 0 to reset bot]'
+	elif placement == '22':
+		return 'These are worth 20 Gralats. One way to catch them is to let them hop, and as they land move and then swing. They are everywhere, but the ones on Castle\'s ramparts are often not caught, so its a good spot. [PM 0 to reset bot]'
+	elif placement == '23':
+		return 'These bugs fly around after being swung at(or just at random every now at then) and then will land again in a different spot, they are worth 52 Gralats. Now, the best technique is just being patient and locking on. [PM 0 to reset bot]'
+	elif placement == '24':
+		return 'Comes from bushes and grass. This bug is worth 16 Gralats. This bug will chase you after spawning and string. The strategy is to get above it, and swing downwards when it is in range. The best spots are in the castle courtyard. [PM 0 to reset bot]'
+	elif placement == '25':
+		return 'Flys around and stops periodically, they are worth 8 Gralats. Just follow it around, swing when it stops and continue following it. The best place(and only place) to find them is Swamp Town. [PM 0 to reset bot]'
+	elif placement == '26':
+		return 'Bugs can be sold to a witch in Swamp Town, she is in the tree above the pond at the start. Everyday she will have a bug she will pay 1.5x the usual price for. Always sell the bugs you get for that price. [PM 0 to reset bot]'
 
 	elif placement == '3':
-		return 'Section under construction please check back later. PM "Metal" to suggest things [PM 0 to reset choices]'
-
+		return '[PM 1 for tips, PM 2 for loot selling locations, PM 0 to reset bot]'
+	elif placement == '31':
+		return "There are 5 loot chests in the Bandit Caves that reset daily. Go get them! [PM 0 to reset bot]"
+	elif placement == '32':
+		return 'PM 1 for selling clothes, 2 for pyrat stuff, 3 for mugs, 4 for weapons, 5 for fishing gear, 6 for sea shells, 7 for odd gem, 8 jewelry, 9 for anything else, and 0 to reset bot]'
+	elif placement == '321':
+		return "Taylor Richaards' House is right under Graal City.[PM 0 to reset choices]"
+	elif placement == '322':
+		return "He is standing on a small boat in York Town. Get in the water east of the horse track and swim east a little.[PM 0 to reset choices]"
+	elif placement == '323':
+		return "Rafael owns the Black Orchid, the pub in northwestern MoD Town.[PM 0 to reset choices]"
+	elif placement == '324':
+		return "The Snow Town Blacksmith, not the Onnet Town one. His place is in the northwest corner of Snow Town.[PM 0 to reset choices]"
+	elif placement == '325':
+		return "He is out fishing next to the house in the southwest corner of Swamp Town.[PM 0 to reset choices]"
+	elif placement == '326':
+		return "It is on the southeast beach of Oasis Island. This island is only accessible to VIP subscribers. If you don't think it is worth buying the VIP subscription, then just sell your seashells to Elster. [PM 0 to reset choices]"
+	elif placement == '327':
+		return "This guy is in the secret furniture in the rail cave between Snow Town and York Town.[PM 0 to reset choices]"
+	elif placement == '328':
+		return "Go to the west side of Destiny. This area is infested with Lizardons, so be careful.  The church is partially caved in, so you have to enter through the hole to the right. [PM 1 to continue]"
+	elif placement == '3281':
+		return 'Inside the church, you have to kill the Lizardon. Then place a bomb up at the front to blow up the ruble blocking the stairs. Father Randell will thank you for rescuing him and will give you a coin. [PM 1 to continue]'
+	elif placement == '32811':
+		return "Enter the cave that is right below Destiny Tower. Inside, go to the right and back. Use the Trick Coin on the metal door and you will be allowed into the Owl's Nest, a blackmarket. [PM 0 to reset choices]"
+	elif placement == '329':
+		return "Elster's Cave is just to the left of Sardon's Tower. If you talk to him, Elster will give you a lump sum of gralats for all your loot items. Note, you won't get as many Gralats as selling to the other NPCs.[PM 0 to reset choices]"
 
 	elif placement == '4':
-		return 'To start, over-world farming is a really good way to earn gralats. It is just boring as hell, so most people stay away from it.[PM 1 to continue, PM 0 to reset choices]'
+		return 'To start, over-world farming is a really good way to earn gralats. It is just boring as hell, so most people stay away from it.[PM 1 to continue, PM 0 to reset bot]'
 	elif placement == '41':
-		return ' VIP island has a good farm in a building in the north west corner, any of the hour areas(Fan, Zone, Snow Lodge, Grotto) are good areas. Then you have that one little house in upper York, I like that one. [PM 1 to continue, PM 0 to reset choices]'
+		return ' VIP island has a good farm in a building in the north west corner, any of the hour areas(Fan, Zone, Snow Lodge, Grotto) are good areas. Then you have that one little house in upper York, I like that one. [PM 1 to continue, PM 0 to reset bot]'
 	elif placement == '411':
-		return 'House farming is a good way to have "back up" money, though most just use it for decoration. I would recommend getting a 2-p mount for this, as it has more blasts per bomb then your average horse.[PM 1 to continue, PM 0 to reset choices]'
+		return 'House farming is a good way to have "back up" money, though most just use it for decoration. I would recommend getting a 2-p mount for this, as it has more blasts per bomb then your average horse.[PM 0 to reset bot]'
 
 	elif placement == '5':
 		return '[PM 1 for info getting the bug net]'
 	elif placement == '51':
-		return 'Open the map and head up to MoD town. When you make it, head down. You will see a tree on your right, go to inside. Talk to Adam(the NPC) inside and receive the bug net. [PM 0 to reset choices]'
+		return 'Open the map and head up to MoD town. When you make it, head down. You will see a tree on your right, go to inside. Talk to Adam(the NPC) inside and receive the bug net. [PM 0 to reset bot]'
 
 	else:
-		return "Error! Valid invalid input. If you think this bot is buggy as crap PM 'Metal'"
+		return "Error! Valid invalid input. PM 0 to reset bot."
+
 main()
